ef1bb38c150ec3c7447503d43311317faab385a8
Amol11 <amolsinghaniya@gmail.com>
2020-02-21 16:25:12 +0530

    phone verification works

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:visitor_testapp/common/widgets/camera.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:visitor_testapp/common/widgets/snackbar.dart';
-import 'package:visitor_testapp/common/widgets/yesNoAlertDialog.dart';
 import 'dart:io';
 import 'package:firebase_auth/firebase_auth.dart';
 
@@ -32,56 +31,27 @@ class LoginPgState extends State<LoginPg> {
 
     final PhoneCodeSent smsCodeSent = (String verId, [int forceCodeResend]) {
       this.verificationId = verId;
+      smsCodeDialog(context).then((value){
+        print('Signed In');
+        signIn();
+      });
+    };
+
+    final PhoneVerificationCompleted verifiedSuccess = (AuthCredential user) {
+      print('verified');
+    };
+
+    PhoneVerificationFailed veriFailed(AuthException exception) {
+      print('${exception.message}');
     };
 
     await FirebaseAuth.instance.verifyPhoneNumber(
         phoneNumber: '+91${_phoneNo.text}',
-        timeout: Duration(seconds: 30),
+        timeout: const Duration(seconds: 0),
         verificationCompleted: verifiedSuccess,
         verificationFailed: veriFailed,
         codeSent: smsCodeSent,
-        codeAutoRetrievalTimeout: autoRetrieve);
-  }
-
-  final PhoneVerificationCompleted verifiedSuccess = (AuthCredential user) {
-    print('verified');
-  };
-
-  PhoneVerificationFailed veriFailed(AuthException exception) {
-    print('${exception.message}');
-  }
-
-  Future<bool> smsCodeDialog(BuildContext context) {
-    return showDialog(
-        context: context,
-        barrierDismissible: false,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            title: Text('Enter the OTP'),
-            content: TextField(
-              onChanged: (value) {
-                this.smsCode = value;
-              },
-            ),
-            contentPadding: EdgeInsets.all(10.0),
-            actions: <Widget>[
-              FlatButton(
-                onPressed: () {
-                  FirebaseAuth.instance.currentUser().then((user) {
-                    if (user != null) {
-                      Navigator.of(context).pop();
-                      //Navigator.of(context).pushReplacement(LoginPg);
-                    } else {
-                      Navigator.of(context).pop();
-                      signIn();
-                    }
-                  });
-                },
-                child: Text('Done'),
-              )
-            ],
-          );
-        });
+        codeAutoRetrievalTimeout: null);
   }
 
   signIn() {
@@ -202,11 +172,10 @@ class LoginPgState extends State<LoginPg> {
                       showSnackBar('Photo required!', _scaffoldKey);
                     else if (_selectedFile != null) {
                       if (_formKey.currentState.validate()) {
-                        verifyPhone();
-//                        showAlert(
-//                            'Do you really want to send an OTP to +91${_phoneNo
-//                                .text} ?', 'Confirmation', context,
-//                            verifyPhone());
+                        //verifyPhone();
+                        showAlert(
+                            'Do you really want to send an OTP to +91${_phoneNo
+                                .text} ?', 'Confirmation', context);
                       }
                     }
                   });
@@ -217,6 +186,83 @@ class LoginPgState extends State<LoginPg> {
         ),
       ),
     );
+  }
+
+  Future<bool> smsCodeDialog(BuildContext context) {
+    return showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('Enter the OTP'),
+            content: TextField(
+              onChanged: (value) {
+                this.smsCode = value;
+              },
+            ),
+            contentPadding: EdgeInsets.all(10.0),
+            actions: <Widget>[
+              FlatButton(
+                onPressed: () {
+                  FirebaseAuth.instance.currentUser().then((user) {
+                    if (user != null) {
+                      Navigator.of(context).pop();
+                      //Navigator.of(context).pushReplacement(LoginPg);
+                    } else {
+                      Navigator.of(context).pop();
+                      signIn();
+                    }
+                  });
+                },
+                child: Text('Done'),
+              )
+            ],
+          );
+        });
+  }
+
+  showAlert(String dialogString, String dialogTitle, context) {
+    AlertDialog dialog = AlertDialog(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      title: Text(
+        dialogTitle,
+        textAlign: TextAlign.center,
+        style: TextStyle(fontSize: 20.0),
+      ),
+      content: Text(
+        dialogString,
+        textAlign: TextAlign.justify,
+      ),
+      actions: <Widget>[
+        FlatButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          child: Text(
+            'No',
+            style: TextStyle(color: Colors.purpleAccent),
+          ),
+        ),
+        FlatButton(
+          onPressed: () {
+            verifyPhone();
+            Navigator.of(context).pop();
+          },
+          child: Text(
+            'Yes',
+            style: TextStyle(color: Colors.purpleAccent),
+          ),
+        ),
+      ],
+    );
+    showDialog(
+        barrierDismissible: false,
+        context: context,
+        builder: (BuildContext context) {
+          return dialog;
+        });
   }
 
   _buttonController() {
